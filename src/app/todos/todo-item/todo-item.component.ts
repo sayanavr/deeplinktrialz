08d7a312151260ae28c59b8cf2430f9b449871a5
sayanavr <sayana.glad@gmail.com>
2021-02-18 16:35:35 -0500

    Facebook and linkedin share

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -8,10 +8,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TodoItemComponent implements OnInit {
   id:any;
+  link:any;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+    this.link="https://www.facebook.com/dialog/share?app_id=249365643438499&display=popup&quote=This+is+the+description+"+this.id+"parameter&href=https%3A%2F%2Fdeeplinktrials.page.link/?link=https%3A%2F%2Fdeeplinktrialz.web.app/todos/"+this.id+"%26st=Item"+this.id+"%26sd=ThisIs"+this.id+"%26si=https%3A%2F%2Fhomepages.cae.wisc.edu/~ece533/images/mountain.png";
   }
 
 }
